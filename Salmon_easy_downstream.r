9e069f588e4f721f45efea7527550e5ac4e4c8ef
bogv127515 <59218801+bogv127515@users.noreply.github.com>
2021-10-14 15:23:09 +0800

    Update Salmon_easy_downstream.r

--- Salmon_easy_downstream.r
+++ Salmon_easy_downstream.r
@@ -1,7 +1,7 @@
 library(tximport)
 library(rtracklayer)
 gtf="gencode.v38.annotation.gtf"
-setwd("./02.Easy_Quanting/")
+setwd("./02.Quanting/")
 gtf <- as.data.frame(import(gtf,format="gtf"))
 tx2gene <- data.frame(TXNAME=gtf$transcript_id,GENEID=gtf$gene_name)
 tx2gene <- tx2gene <- tx2gene[-is.na(tx2gene$TXNAME),]
