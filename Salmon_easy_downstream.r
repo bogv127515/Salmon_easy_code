fcc43caedbe1017209007df55984682e0173c8ee
bogv127515 <59218801+bogv127515@users.noreply.github.com>
2021-10-14 15:09:24 +0800

    Create Salmon_easy_downstream.r

---
+++ Salmon_easy_downstream.r
@@ -0,0 +1,13 @@
+library(tximport)
+library(rtracklayer)
+gtf="gencode.v38.annotation.gtf"
+setwd("./02.Easy_Quanting/")
+gtf <- as.data.frame(import(gtf,format="gtf"))
+tx2gene <- data.frame(TXNAME=gtf$transcript_id,GENEID=gtf$gene_name)
+tx2gene <- tx2gene <- tx2gene[-is.na(tx2gene$TXNAME),]
+
+file_name <- list.files("./")
+file_name <- paste0(file_name,"/quant.sf")
+#对于Salmon-CDS file_name <- paste0(file_name,"/quant.sf.new")
+quant_data <- tximport(files = file_name,type = "salmon",tx2gene = tx2gene)
+count_matrix <- round(quant_data$counts)
